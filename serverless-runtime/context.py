62d05e119f6ad618f0c0cae1b9a16d271315e76b
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-01-18 11:36:49 -0300

    feat: defining a context class

---
+++ serverless-runtime/context.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+
+
+class Context(object):
+    host = None
+    port = None
+    input_key = None
+    output_key = None
+    last_execution = None
+    env = None
+
+    def __init__(self, host: str = "localhost", port: int = 6379, input_key: str = "metrics", output_key: str = "some_output"):
+        self.host = host
+        self.port = port
+        self.input_key = input_key
+        self.output_key = output_key
+        self.function_getmtime = datetime.datetime.fromtimestamp(os.path.getmtime("/app/handler.py"))
+        self.last_execution = None
+        self.env = {}
+    
+    def set_env(self, data: dict):
+        self.env = data
+    
+    def update_last_execution(self):
+        self.last_execution = datetime.datetime.now()
