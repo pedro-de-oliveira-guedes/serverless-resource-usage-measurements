8e32e4876c32772bfccc28d84d37c0dcf9aa5ef2
pedro-de-oliveira-guedes <pedro.og2002@gmail.com>
2025-01-18 18:28:28 -0300

    feat: getting function_getmtime from parameterized path

--- serverless-runtime/context.py
+++ serverless-runtime/context.py
@@ -10,12 +10,19 @@ class Context(object):
     last_execution = None
     env = None
 
-    def __init__(self, host: str = "localhost", port: int = 6379, input_key: str = "metrics", output_key: str = "some_output"):
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: int = 6379,
+        input_key: str = "metrics",
+        output_key: str = "some_output",
+        main_file_path: str = "/app/handler.py",
+    ):
         self.host = host
         self.port = port
         self.input_key = input_key
         self.output_key = output_key
-        self.function_getmtime = datetime.datetime.fromtimestamp(os.path.getmtime("/app/handler.py"))
+        self.function_getmtime = datetime.datetime.fromtimestamp(os.path.getmtime(main_file_path))
         self.last_execution = None
         self.env = {}
     
